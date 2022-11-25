cf35aa6b78979934d2d9a63392c256feb6db7bf1
Guilherme Saad <guilherme.saad@ufu.br>
2022-11-25 10:57:35 -0300

    commit trabalhos2

--- aula2/somaSeqHailst.c
+++ aula2/somaSeqHailst.c
@@ -5,3 +5,43 @@
  * soma sequencial do Hailst
  */
 
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool impar(int n);
+
+int main(){
+    int numero, soma =0;
+    printf("Digite seu numero inicial\n");
+    scanf("%d",&numero);
+    soma = numero;
+
+    while(numero>1){
+        
+        if(impar(numero)){
+            numero = 3*numero + 1;
+            soma += numero;
+        }
+
+        else{
+            numero = numero/2;
+            soma += numero;
+        }
+
+    }
+
+    printf("%d", soma);
+
+
+
+
+return 0;
+}
+
+bool impar(int n){
+    if(n%2)
+        return true;
+    return false;
+
+}
