98d763d2d829d3cc0486eadb1958b93cf2d23467
Guilherme Saad <guilherme.saad@ufu.br>
2022-11-29 14:13:01 -0300

    commit lab2

--- aula2/maxHappyPrime.c
+++ aula2/maxHappyPrime.c
@@ -9,89 +9,63 @@
 #include <stdbool.h>
 #include <math.h>
 
-bool primo(int a);
-bool feliz(int a);
-int tamanho(int a);
+bool primo(int n);
+int feliz1(int n);
+bool feliz2(int n);
 
-int main(int argc, char** argv){
-	
-    int n, r;
-    r = -1;
-    printf("coloque o n:");
+int main(){
+
+    int n = 0;
+    printf("Digite seu numero: \n");
     scanf("%d", &n);
-    
-    for(int i = 2; i<=n;i++){
-        if(primo(i) && feliz(i)){
-            r=i;
-        }
+
+    if(n<7){
+            printf("Nenhum numero foi encontrado\n");
     }
-    if(r ==-1){
-        printf("0***\n");
-    }else{
-        printf("%d\n", r);
-    }
+    else{
+        while(1){
 
-	return 0;
-}
-
-bool primo(int a){
-    for(int i=2; i<a;i++){
-        if(a%i == 0){
-            return false;
-        }
-    }
-    return true;
-}
-
-bool feliz(int a){
-
-    int n;
-    int s = 0;
-    int c;
-    while(true){
-        int b[100];
-        s = 0;
-
-        for (int j = tamanho(a); j>0;j--){
-            c = a/pow(10, j-1);
-            n = c %10;
-            b[j-1] = n;
-            
-        }
-
-        for (int i=0; i<tamanho(a); i++){
-            s = s + (b[i]*b[i]);
-        }
-        a = s;
-        
-        if(s == 4){
-            return false;
-        }else if(s == 1){
-            return true;
-        }
-
-    }
-}
-
-int tamanho(int a){
-    int i, s, t;
-    i = 0;
-    if(a <10){
-        t = 1;
-    }else{
-
-        while(true){
-            s = a/10;
-            i++;
-            a = s;
-            if(s <10){
-                i++;
+            if(primo(n) && feliz2(n)){
+                printf("%d", n);
                 break;
             }
+            n=n-1;
         }
-
-        t = i;
     }
-    return t;
+return 0;
+}
 
+bool primo(int n){
+    for(int i=2; i<n; i++){
+        if(!(n%i))
+            
+            return false;
+    }
+    if(n==1)
+    return false;
+    else
+    return true;
+    
+}
+
+int feliz1(int n){    
+    int rem = 0, sum = 0;    
+          
+    while(n > 0){    
+        rem = n%10;    
+        sum = sum + (rem*rem);    
+        n = n/10;    
+    }    
+    return sum;    
+}
+
+bool feliz2(int n){
+    while(n != 1 && n != 4){
+        n = feliz1(n);
+    }
+    if(n == 1)    
+        return true;  
+  
+    if(n == 4)    
+        return false;
 }
